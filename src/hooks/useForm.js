6cf02e9c04b5d54104488df885ca035f98b4354c
alex16leiva <alex16leiva@gmail.com>
2025-11-26 18:55:42 -0600

    feat: agregar el hook, useForm

---
+++ src/hooks/useForm.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export const useForm = (initialForm = {}) => {
+
+    const [formState, setFormState] = useState(initialForm)
+
+    const onInputChange = (e) => {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    };
+
+    const onResetForm = (newValues = initialForm) => {
+        setFormState(newValues);
+    };
+
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        onResetForm,
+    }
+}
