9582d9313ddab56725ed6dd0d9b5bc36c0a610ae
alex16leiva <alex16leiva@gmail.com>
2025-11-26 18:50:44 -0600

    chore(store): configurar controlsSlicer en Redux

--- src/app/stores/store.js
+++ src/app/stores/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loadingReducer from "../../components/Controls/WaitControl/loadingSlice";
 import authReducer from '../../features/auth/authSlice';
+import controlsSlicer from "../../components/Controls/ReducerControl/controlsSlicer";
+
+
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         loading: loadingReducer,
+        controls: controlsSlicer
     },
 });
