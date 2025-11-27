1d28d67e60b72dc188cc1ea2d3ed624ce75a3c96
alex16leiva <alex16leiva@gmail.com>
2025-11-26 18:53:30 -0600

    feat(controls): agregar componente SearchControl

---
+++ src/components/Controls/SearchControl/index.jsx
@@ -0,0 +1,50 @@
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } from '@mui/material'
+import { GridSearchIcon } from '@mui/x-data-grid'
+import PropTypes from "prop-types";
+
+export const SearchControl = ({
+    width,
+    textSearch = 'Search',
+    onBlur,
+    onChange,
+    onKeyDown,
+    value,
+    onClick
+}) => {
+    return (
+        <FormControl sx={{ width: `${width}px` }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">{textSearch}</InputLabel>
+            <OutlinedInput
+                id="outlined-adornment-password"
+                type={'text'}
+                endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={onClick}
+                            edge="end"
+                        >
+                            <GridSearchIcon />
+                        </IconButton>
+                    </InputAdornment>
+                }
+                label={textSearch}
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                onKeyDown={onKeyDown}
+                sx={{ height: '40px', padding: '0 10px' }}
+            />
+        </FormControl>
+    )
+}
+
+SearchControl.propTypes = {
+    width: PropTypes.number,
+    textSearch: PropTypes.string,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    onKeyDown: PropTypes.func,
+    onClick: PropTypes.func,
+    value: PropTypes.string.isRequired
+};
